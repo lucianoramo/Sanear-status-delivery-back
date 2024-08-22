87a54a7a7a295041f9c66bf3beeb791aa57be9b1
Luciano Gazai <luciano.ramo76@gmail.com>
2024-08-21 21:32:28 -0300

    fix for aws

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const corsOptions: CorsOptions = {
-    origin: ['http://localhost:3000'], // Allow only this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // If you need to allow cookies or authentication headers
-  };
-
-  app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
