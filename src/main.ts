40c670a80be47b78053f79e31cf083e88c9f0989
Luciano Gazai <luciano.ramo76@gmail.com>
2024-09-04 20:40:50 -0300

    change port back to 8080

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import fastifyCors from '@fastify/cors';
 import fastifyMultipart from '@fastify/multipart';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
-  await app.register(fastifyCors, {
-    origin: '*', // Or '*' to allow all origins
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    credentials: true,
-  });
+	await app.register(fastifyCors, {
+		origin: '*', // Or '*' to allow all origins
+		methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+		credentials: true,
+	});
 
-  await app.register(fastifyMultipart);
+	await app.register(fastifyMultipart);
 
-  await app.listen(8083, '0.0.0.0');
+	await app.listen(8080, '0.0.0.0');
 }
 bootstrap();
